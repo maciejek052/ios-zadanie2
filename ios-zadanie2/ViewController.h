d96760212c332277f9d77c9d99443bfd8cf8423a
maciejek052 <maciejek052@gmail.com>
2021-11-09 21:36:18 +0100

    Initial Commit

---
+++ ios-zadanie2/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  ios-zadanie2
+//
+//  Created by Maciek on 09/11/2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ios-zadanie2/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  ios-zadanie2
+//
+//  Created by Maciek on 09/11/2021.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
